0404a75529d3f3697a2f5b72c713907a31a520d2
LorneH12 <lorneh12@gmail.com>
2025-11-20 17:18:56 -0800

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -2,7 +2,7 @@
 // Simple event tracking to your Google Apps Script endpoint
 
 const ANALYTICS_ENDPOINT =
-  "https://script.google.com/macros/s/AKfycbwu1hSGEC0V1eGy8ShHFhCGqRi5VjAUbSoppgUIrVJCwxdOuQiD04SZKPf-0KltR0Go/exec";
+  "https://script.google.com/macros/s/AKfycbyDYDlaYYcTp87Ps2Be5harfGXz6zcOBL1ad4099qvemZEADM2IDt0LCS12RHbnISM/exec";
 
 function trackEvent(eventType, data = {}) {
   const payload = {
